c930741a26dbeae97619eb1ea5e3acb21c1b7b2e
campishome <campnapat007@gmail.com>
2024-03-09 11:12:21 +0700

    fixed

--- dbconnect.js
+++ dbconnect.js
@@ -1,20 +1,20 @@
 // import mysql from "mysql";
-// import util from "util";
+// // import util from "util";
 
-const sql = require('mysql2');
-const pool = sql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit:0
-})
+// const sql = require('mysql2');
+// const pool = sql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit:0
+// })
 
-pool.getConnection((err,conn)=>{
-    if(err) console.log(err)
-    console.log("Connected succesfuly");
-})
+// pool.getConnection((err,conn)=>{
+//     if(err) console.log(err)
+//     console.log("Connected succesfuly");
+// })
 
-module.exports = pool.promis()
+// module.exports = pool.promis()
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const mysql = require('mysql'); // เพิ่มบรรทัดนี้
+require('dotenv').config();
 const app = express();
 const PORT = 4000;
+
+const connection = mysql.createConnection({
+    host:"202.28.34.197",
+    user:"web66_65011212009",
+    password:"65011212009@csmsu",
+    database:"web66_65011212009"
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('เกิดข้อผิดพลาดในการเชื่อมต่อกับ MySQL: ' + err.stack);
+        return;
+    }
+
+    console.log('เชื่อมต่อกับ MySQL สำเร็จ');
+});
 
 app.get('/', (req, res) => {
     res.send('This is my API, but from try.ts');
 });
 
-app.get('/movie',async (req, res) => {
-    await pool.query('SELECT * FROM Movie', (err, result, fields)=>{
-        res.json(result);
+app.get('/api/movies', (req, res) => {
+    // คำสั่ง SQL สำหรับการเลือกข้อมูล
+    const sql = 'SELECT * FROM Movie';
+
+    // ดึงข้อมูล
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('เกิดข้อผิดพลาดในการดึงข้อมูล: ' + err.stack);
+            return res.status(500).json({ error: 'เกิดข้อผิดพลาดในการดึงข้อมูล' });
+        }
+
+        res.json(results);
     });
 });
 
