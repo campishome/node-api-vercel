73d2364f243e35513650886704b1ffee695a3af2
campishome <campnapat007@gmail.com>
2024-03-09 09:49:26 +0700

    db fixed

--- try.ts
+++ try.ts
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 const PORT = 4000;
-
 import { router as movie } from "./api/movie";
 
-module.exports = app;
+app.get('/',(req,res)=>{
+    res.send('This is my API,But from try.ts');
+})
+
 app.use("/movie", movie);
+
+module.exports = app;
