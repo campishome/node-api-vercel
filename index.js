aa7c343fc9da291c465ca5d0584028a41200f38c
campishome <campnapat007@gmail.com>
2024-03-09 11:19:50 +0700

    fixed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 4000;
 
 const connection = mysql.createConnection({
+    connectionLimit:10,
     host:"202.28.34.197",
     user:"web66_65011212009",
     password:"65011212009@csmsu",
@@ -16,8 +17,16 @@ connection.connect((err) => {
         console.error('เกิดข้อผิดพลาดในการเชื่อมต่อกับ MySQL: ' + err.stack);
         return;
     }
-
     console.log('เชื่อมต่อกับ MySQL สำเร็จ');
+    // ทดสอบ query ข้อมูล
+    connection.query('SELECT 1 + 1 AS solution', (err, results) => {
+        if (err) {
+            console.error('เกิดข้อผิดพลาดในการ query ข้อมูล: ' + err.stack);
+            return;
+        }
+
+        console.log('ผลลัพธ์: ', results[0].solution);
+    });
 });
 
 app.get('/', (req, res) => {
@@ -34,9 +43,9 @@ app.get('/api/movies', (req, res) => {
             console.error('เกิดข้อผิดพลาดในการดึงข้อมูล: ' + err.stack);
             return res.status(500).json({ error: 'เกิดข้อผิดพลาดในการดึงข้อมูล' });
         }
-
         res.json(results);
     });
 });
 
 module.exports = app;
+connection.end();
